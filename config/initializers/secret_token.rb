e4e5e1f54dca25b55db20587f219e9adea5ae87a
Tony Hsu <tony@TonydeMacBook-Air.local>
2014-02-05 20:13:08 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsFreshman::Application.config.secret_key_base = 'a9a216126f04f6c07937c734ffae2051bdc4c2f61bba328facf47eb35fc978a9e0a94ef85820a69c5f268ae599dbb3e6a7d0b0090b5165aef84b9f4d8ece0249'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsFreshman::Application.config.session_store :cookie_store, key: '_rails_freshman_session'
